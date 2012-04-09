a485fc6c45fe4221106d4d2cf982b20c1d047410
michaelD <michael@wibiya.com>
2012-04-09 16:29:59 +0300

    newChangelist

---
+++ testDirectory/testClass.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: michael.dragatzki
+ * Date: 4/9/12
+ * Time: 4:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class testClass
+{
+
+}
